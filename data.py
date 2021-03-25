eaa4caa6254ea1a30db1ad0c5fe0e8f4e1fba088
WillCPP <WillCPP@users.noreply.github.com>
2021-03-25 02:23:08 -0400

    Update data.py

--- data.py
+++ data.py
@@ -1,24 +1,27 @@
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
+import numpy as np
+from sklearn.model_selection import train_test_split
 
-# vocab_size = 20000  # Only consider the top 20k words
-# num_tokens_per_example = 200  # Only consider the first 200 words of each movie review
-(x_train, y_train), (x_val, y_val) = keras.datasets.imdb.load_data()  # num_words=vocab_size)
+# (x_train, y_train), (x_val, y_val) = keras.datasets.imdb.load_data()  # num_words=vocab_size)
+# print('X ==========')
+# print(x_train.shape)
+# print(type(x_train))
+# print(x_train[0])
+# print(type(x_train[0]))
+# print(x_train[0][0])
+# print('Y ==========')
+# print(y_train.shape)
+# print(type(y_train))
+# print(y_train[0])
+# print(type(y_train[0]))
 
 
-print('X ==========')
-print(x_train.shape)
-print(type(x_train))
-
-print(x_train[0])
-print(type(x_train[0]))
-
-print(x_train[0][0])
-
-print('Y ==========')
-print(y_train.shape)
-print(type(y_train))
-
-print(y_train[0])
-print(type(y_train[0]))
+ds_data = np.load('dataset/data.npy', allow_pickle=True)
+ds_labels = np.load('dataset/labels.npy', allow_pickle=True)
+print(ds_data.shape)
+print(ds_labels.shape)
+x_train, x_val, y_train, y_val = train_test_split(ds_data, ds_labels, train_size=0.01)
+np.save(f'dataset_2/data', x_train)
+np.save(f'dataset_2/labels', y_train)
